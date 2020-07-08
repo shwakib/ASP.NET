b61fe6b65cb3c5f7aa3350bf5b2bad177974256f
Sifat Hasan Wakib <sifat6460@gmail.com>
2020-07-08 17:02:47 +0600

    1st

---
+++ firstlabclasspractice/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstlabclasspractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "It is working";
+        }
+
+        public ActionResult Index1(int? id = null)
+        {
+            return RedirectToAction("anothermethod");
+        }
+
+        public string anothermethod(int? id=null)
+        {
+            if(id==null)
+            {
+                return "reponse from other method";
+            }
+            return "response from another method: "+id.ToString();
+        }
+
+        public ViewResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ firstlabclasspractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="firstlabclasspractice.MvcApplication" Language="C#" %>
